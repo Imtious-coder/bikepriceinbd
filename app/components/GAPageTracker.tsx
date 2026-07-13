d638ecd8d90a70e04a71ccb5caec7b0f175ad929
Imtious-Coder <iarafat386@gmail.com>
2026-07-14 00:42:05 +0600

    GA$ add test 1

---
+++ app/components/GAPageTracker.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { usePathname, useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
+
+declare global {
+  interface Window {
+    gtag: (...args: any[]) => void;
+  }
+}
+
+export default function GAPageTracker() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    if (!window.gtag) return;
+    const url = pathname + (searchParams.toString() ? `?${searchParams.toString()}` : '');
+    window.gtag('config', process.env.NEXT_PUBLIC_GA_ID as string, {
+      page_path: url,
+    });
+  }, [pathname, searchParams]);
+
+  return null;
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import GoogleAnalytics from "./components/GoogleAnalytics";
+import { Suspense } from "react";
+import GAPageTracker from "./components/GAPageTracker";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +19,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children} <GoogleAnalytics />
+        <Suspense fallback={null}>
+          <GAPageTracker />
+        </Suspense>
+      </body>
     </html>
   );
 }
